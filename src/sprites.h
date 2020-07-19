7a93c5cde45e183bdf980d8fbc96ef9416253f39
kvbc <64214044+kvbc@users.noreply.github.com>
2020-07-19 11:20:28 +0200

    Update sprites.h

--- src/sprites.h
+++ src/sprites.h
@@ -1,8 +1,8 @@
 #pragma once
 
 
-#define SPRITE_PLAYER								 \
-			"    \xDB    ",							 \
+#define SPRITE_PLAYER						 \
+			"    \xDB    ",				 \
 			"\xDB\xDC\xDC\xDB\xDB\xDB\xDC\xDC\xDB",  \
 			"\xDB  \xDB\xDB\xDB  \xDB"
 			/*
@@ -29,9 +29,9 @@
 
 #define SPRITE_GAMEOVER \
 			" \xDB\xDB\xDB\xDB\xDB\xDB   \xDB\xDB\xDB\xDB\xDB  \xDB\xDB\xDB    \xDB\xDB\xDB \xDB\xDB\xDB\xDB\xDB\xDB\xDB      \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB    \xDB\xDB \xDB\xDB\xDB\xDB\xDB\xDB\xDB \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB", \
-			"\xDB\xDB       \xDB\xDB   \xDB\xDB \xDB\xDB\xDB\xDB  \xDB\xDB\xDB\xDB \xDB\xDB          \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB      \xDB\xDB   \xDB\xDB \xDB\xDB", \
-			"\xDB\xDB   \xDB\xDB\xDB \xDB\xDB\xDB\xDB\xDB\xDB\xDB \xDB\xDB \xDB\xDB\xDB\xDB \xDB\xDB \xDB\xDB\xDB\xDB\xDB       \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB\xDB\xDB\xDB   \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB", \
-			"\xDB\xDB    \xDB\xDB \xDB\xDB   \xDB\xDB \xDB\xDB  \xDB\xDB  \xDB\xDB \xDB\xDB          \xDB\xDB    \xDB\xDB  \xDB\xDB  \xDB\xDB  \xDB\xDB      \xDB\xDB   \xDB\xDB   ", \
+			"\xDB\xDB       \xDB\xDB   \xDB\xDB \xDB\xDB\xDB\xDB  \xDB\xDB\xDB\xDB \xDB\xDB          \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB      \xDB\xDB   \xDB\xDB \xDB\xDB", 						   \
+			"\xDB\xDB   \xDB\xDB\xDB \xDB\xDB\xDB\xDB\xDB\xDB\xDB \xDB\xDB \xDB\xDB\xDB\xDB \xDB\xDB \xDB\xDB\xDB\xDB\xDB       \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB\xDB\xDB\xDB   \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB", 	   \
+			"\xDB\xDB    \xDB\xDB \xDB\xDB   \xDB\xDB \xDB\xDB  \xDB\xDB  \xDB\xDB \xDB\xDB          \xDB\xDB    \xDB\xDB  \xDB\xDB  \xDB\xDB  \xDB\xDB      \xDB\xDB   \xDB\xDB   ", 							   \
 			" \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB   \xDB\xDB \xDB\xDB      \xDB\xDB \xDB\xDB\xDB\xDB\xDB\xDB\xDB      \xDB\xDB\xDB\xDB\xDB\xDB    \xDB\xDB\xDB\xDB   \xDB\xDB\xDB\xDB\xDB\xDB\xDB \xDB\xDB   \xDB\xDB \xDB\xDB"
 			/*
 				 ######   #####  ###    ### #######      ######  ##    ## ####### ######  ##
@@ -46,9 +46,9 @@
 
 #define SPRITE_YOUWON \
 			"\xDB\xDB    \xDB\xDB  \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB    \xDB\xDB     \xDB\xDB     \xDB\xDB  \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB\xDB    \xDB\xDB \xDB\xDB", \
-			" \xDB\xDB  \xDB\xDB  \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB     \xDB\xDB     \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB\xDB\xDB   \xDB\xDB \xDB\xDB", \
-			"  \xDB\xDB\xDB\xDB   \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB     \xDB\xDB  \xDB  \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB \xDB\xDB  \xDB\xDB \xDB\xDB", \
-			"   \xDB\xDB    \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB     \xDB\xDB \xDB\xDB\xDB \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB  \xDB\xDB \xDB\xDB   ", \
+			" \xDB\xDB  \xDB\xDB  \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB     \xDB\xDB     \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB\xDB\xDB   \xDB\xDB \xDB\xDB", 	       \
+			"  \xDB\xDB\xDB\xDB   \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB     \xDB\xDB  \xDB  \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB \xDB\xDB  \xDB\xDB \xDB\xDB",       \
+			"   \xDB\xDB    \xDB\xDB    \xDB\xDB \xDB\xDB    \xDB\xDB     \xDB\xDB \xDB\xDB\xDB \xDB\xDB \xDB\xDB    \xDB\xDB \xDB\xDB  \xDB\xDB \xDB\xDB   ",             \
 			"   \xDB\xDB     \xDB\xDB\xDB\xDB\xDB\xDB   \xDB\xDB\xDB\xDB\xDB\xDB       \xDB\xDB\xDB \xDB\xDB\xDB   \xDB\xDB\xDB\xDB\xDB\xDB  \xDB\xDB   \xDB\xDB\xDB\xDB \xDB\xDB"
 			/*
 				##    ##  ######  ##    ##     ##     ##  ######  ###    ## ## 
@@ -63,9 +63,9 @@
 
 /*
 // 57, 5
-#define SPACESHOOTER_SPRITE														 \
-			"   ____                    ___ _              _          ",		 \
-			"  / __/__  ___ ________   / __/ /  ___  ___  / /____ ____",		 \
+#define SPACESHOOTER_SPRITE								     \
+			"   ____                    ___ _              _          ",	     \
+			"  / __/__  ___ ________   / __/ /  ___  ___  / /____ ____",	     \
 			" _\\ \\/ _ \\/ _ `/ __/ -_) _\\ \\/ _ \\/ _ \\/ _ \\/ __/ -_) __/", \
 			"/___/ .__/\\_,_/\\__/\\__/ /___/_//_/\\___/\\___/\\__/\\__/_/   ",  \
 			"   /_/                                                   "
@@ -73,7 +73,7 @@
 
 /*
 // 56, 5
-#define SPACESHOOTER_SPRITE												\
+#define SPACESHOOTER_SPRITE							    \
 			" _____                    _____ _           _           ", \
 			"|   __|___ ___ ___ ___   |   __| |_ ___ ___| |_ ___ ___ ", \
 			"|__   | . | .'|  _| -_|  |__   |   | . | . |  _| -_|  _|", \
@@ -82,12 +82,12 @@
 */
 
 // 70, 6
-#define SPRITE_SPACESHOOTER																	    \
-			"  ____                           ____  _                 _            ",			\
-			" / ___| ____   ____  ___ ___    / ___|| |__   ___   ___ | |_ ___ ____ ",			\
+#define SPRITE_SPACESHOOTER										    \
+			"  ____                           ____  _                 _            ",	    \
+			" / ___| ____   ____  ___ ___    / ___|| |__   ___   ___ | |_ ___ ____ ",	    \
 			" \\___ \\|  _ \\ / _  |/ __/ _ \\   \\___ \\|  _ \\ / _ \\ / _ \\| __/ _ \\  __|", \
-			"  ___) | |_) | (_| | (_|  __/    ___) | | | | (_) | (_) | ||  __/ |   ",			\
-			" |____/|  __/ \\____|\\___\\___|   |____/|_| |_|\\___/ \\___/ \\__\\___|_|   ",	\
+			"  ___) | |_) | (_| | (_|  __/    ___) | | | | (_) | (_) | ||  __/ |   ",	    \
+			" |____/|  __/ \\____|\\___\\___|   |____/|_| |_|\\___/ \\___/ \\__\\___|_|   ",    \
 			"       |_|                                                          "
 			/*
 				 ____                           ____  _                 _
@@ -95,7 +95,7 @@
 				\___ \|  _ \ / _  |/ __/ _ \   \___ \|  _ \ / _ \ / _ \| __/ _ \  __|
 				 ___) | |_) | (_| | (_|  __/    ___) | | | | (_) | (_) | ||  __/ |
 				|____/|  __/ \____|\___\___|   |____/|_| |_|\___/ \___/ \__\___|_|
-					  |_|
+				      |_|
 			*/
 #define SPACESHOOTER_SIZE_X 70
 #define SPACESHOOTER_SIZE_Y 6
